5460c43860e79c1b0f79ce6b3add3f23dff08c54
Kajalfultariya <95041979+Kajalfultariya@users.noreply.github.com>
2021-12-29 11:53:49 +0530

    Add files via upload

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,6 +5,7 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
    
     return (
+        <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <a className="navbar-brand" href="/">Foody User</a>
             <div className="container">
@@ -26,13 +27,39 @@ const Header = () => {
                             </li>
                             <li className="nav-item">
                                 <NavLink className="nav-link" exact to="/Home">All Records </NavLink>
-                            </li>                          
+                            </li>  
+                            <li className="nav-item">
+                                <NavLink className="nav-link" exact to="/ChkBox">Check Box</NavLink>
+                            </li>  
+                            <li className="nav-item">
+                                <NavLink className="nav-link" exact to="/UImage">Image Zip</NavLink>
+                            </li>        
+                            <li className="nav-item">
+                                <NavLink className="nav-link" exact to="/About">Multiple Image Preview</NavLink>
+                            </li>       
+                            <li className="nav-item">
+                                <NavLink className="nav-link" exact to="/ImgUp">Single Image Download </NavLink>
+                            </li>                           
                         </ul>
 
                     </div>
                 </div>
+               
             </div>
+            
         </nav>
+        
+        <nav>
+        <div className="sidenav" >
+        <ul className="navbar-nav me-auto">
+                         
+                            <li className="nav-item">
+                                <NavLink className="nav-link" exact to="/WysiwygRteDraft">Wysiwyg Draft To HTML</NavLink>
+                            </li>
+                           
+                            </ul>
+    </div></nav>
+    </div>
     )
 }
 
